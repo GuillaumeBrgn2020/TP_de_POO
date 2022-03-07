0767fef13fa21a2771190c868f030c38aa0dbe7b
Guillaume <guillaume.bourgeon@telecomnancy.eu>
2022-03-07 10:10:47 +0100

    TP de POO

---
+++ TD types/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+
+    public Animal f(Animal z, Canard t){
+        Canard unCanard;
+        if(!this.estUneSorteDe(z)){
+            return(z);
+        }
+        if 
+    }
+    
+}
